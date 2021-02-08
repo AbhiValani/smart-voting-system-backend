6be0ca38dd999431e4388ea423b6fe7016a75fba
AbhiValani <77375202+AbhiValani@users.noreply.github.com>
2021-02-08 12:20:55 +0530

    first commit

---
+++ src/test/java/com/example/smartvotingsystem/SmartVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartvotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
